1ed1886f8a610e80d2b84a94e415e211caa012c0
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-04-03 12:30:34 +0530

    Dashboardcontroller fixed

---
+++ src/main/java/com/internship_zorvyn/demo/controller/DashboardController.java
@@ -0,0 +1,31 @@
+package com.internship_zorvyn.demo.controller;
+import com.internship_zorvyn.demo.service.FinancialService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+
+    private final FinancialService service;
+
+    public DashboardController() {
+        this.service = null;
+    }
+
+    @GetMapping("/summary")
+    public Map<String, Object> summary() {
+
+        Double income = service.getIncome();
+        Double expense = service.getExpense();
+
+        return Map.of(
+                "income", income,
+                "expense", expense,
+                "net", income - expense
+        );
+    }
+}
