9ddaa10ee83c175ae8b5f7aa56002c25aa986a55
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-04-03 14:06:41 +0530

    Exception fixed

--- src/main/java/com/internship_zorvyn/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/internship_zorvyn/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.internship_zorvyn.demo.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
