b58b38534be242d34bf180ad7de83ef9c8aec60b
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-04-03 11:35:46 +0530

    Strucuted finalized MVC Layer

---
+++ src/main/java/com/internship_zorvyn/demo/model/Role.java
@@ -0,0 +1,5 @@
+package com.internship_zorvyn.demo.model;
+
+public enum Role {
+    ADMIN, ANALYST, VIEWER
+}
---
+++ src/main/java/com/internship_zorvyn/demo/model/Status.java
@@ -0,0 +1,5 @@
+package com.internship_zorvyn.demo.model;
+
+public enum Status {
+    ACTIVE, INACTIVE
+}
